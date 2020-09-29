9403e71377f76bcbaaa64ccaa0dac13a71c74b5d
salihcan <salihcn.bnb@gmail.com>
2020-09-29 13:40:10 +0300

    Custom dialog design completed

--- lib/ui/screen/HomeScreen.dart
+++ lib/ui/screen/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_alertdialog/ui/components/CustomAlertDialog.dart';
 
@@ -18,19 +19,57 @@ class _HomeScreenState extends State<HomeScreen> {
           child: RaisedButton(
             child: Text("Open Dialog"),
             onPressed: () {
-              CustomAlertDialog(
-                context: context,
-                title: "Test Amaçlı Titledir",
-                description: "Test Amaçlı Açıklamadır",
-                onClose: () {
-                  print('CUSTOM ALERT DİALOG KAPANDI');
-                },
-                buttonType: DialogButtonType.SINGLE_BUTTON
-              ).show();
+              openDialog();
             },
           ),
         ),
       ),
     );
+  }
+
+  void openDialog() {
+    CustomAlertDialog(
+      context: context,
+      title: "Rezervasyon İptali",
+      titleTextColor: Colors.black,
+      titleTextSize: 18,
+      titleTextFontWeight: FontWeight.normal,
+      description: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Rezervasyon iptal edilecektir."),
+          Text("Devam Edilsin mi ?"),
+        ],
+      ),
+      singleButtonTitle: "Kapat",
+      singleButtonColor: Colors.blue,
+      singleButtonFontSize: 17,
+      singleButtonTextColor: Colors.white,
+      buttonType: DialogButtonType.MULTI_BUTTON,
+      multiButtonAlignment: Alignment.center,
+      buttonList: [
+        Container(
+          width: MediaQuery.of(context).size.width * .3,
+          child: FlatButton(
+            child: Text("Hayır"),
+            onPressed: () => {},
+          ),
+        ),
+        SizedBox(
+          width: 2,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * .3,
+          child: RaisedButton(
+            child: Text(
+              "Evet",
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.redAccent,
+            onPressed: () => {},
+          ),
+        ),
+      ],
+    ).show();
   }
 }
